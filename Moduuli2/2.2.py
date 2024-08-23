d74dff4f3fe948857b9f9f157e5f01444ddf48a4
Ira <ira.kumpula@metropolia.fi>
2024-08-23 17:40:35 +0300

    2.2 tehty

--- Moduuli2/2.2.py
+++ Moduuli2/2.2.py
@@ -1,3 +1,12 @@
 # Tehtävä 2.2.
 # Kirjoita ohjelma, joka kysyy ympyrän säteen ja tulostaa sen pinta-alan.
 
+#ympyrän pinta-alan kaava on pii*r^2 eli math.pi*r**2
+
+import math
+
+r = input("Kerro ympyrän säde: ")
+area_circle = math.pi*float(r)**2   #muuttuja r on merkkijono, muutos liukuluvuksi
+print("Ympyrän pinta-ala on " + str(area_circle))   #liukuluvun muutos merkkijonoksi
+
+#saman tulosteen saa myös seuraavalla: print(f"Ympyrän pinta-ala on {area_circle}")
