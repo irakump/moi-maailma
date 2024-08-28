3ebc927487563b70899324a01daf749a2d84d511
Ira <ira.kumpula@metropolia.fi>
2024-08-28 19:38:11 +0300

    mod3 tehtävät

--- Moduuli3/Moduuli3_tehtavat/3.4.py
+++ Moduuli3/Moduuli3_tehtavat/3.4.py
@@ -6,17 +6,17 @@
 year = int(input("Kerro vuosiluku: "))      #kysytään vuosiluku
 four = (year/ 4)                           #jaetaan vuosiluku neljällä
 
-if (four.is_integer() == True):             #kokeillaan, onko luku jaollinen neljällä
+if (four.is_integer()):             #kokeillaan, onko luku jaollinen neljällä #ei tarvi olla == True !
     hundred = (year / 100)                  #vuosiluku jaetaan sadalla
-    if (hundred.is_integer() == True):      #kokeillaan, onko vuosiluku jaollinen sadalla
+    if (hundred.is_integer()):      #kokeillaan, onko vuosiluku jaollinen sadalla
         fourhundred = (year / 400)          #jaetaan vuosiluku 400:lla
-        if ((hundred.is_integer() == True) and (fourhundred.is_integer() == True)):  #jos jaollinen 100 ja 400
+        if ( (hundred.is_integer()) and (fourhundred.is_integer()) ):  #jos jaollinen 100 ja 400, mol. totta
             print("Vuosi on karkausvuosi.")
         else:                               #jos sadalla jaollinen luku ei ole jaollinen 400:lla
             print("Vuosi ei ole karkausvuosi.")
     else: print("Vuosi on karkausvuosi.")   #jos vuosiluku ei ole jaollinen sadalla, on silti karkausvuosi
 else:
-    print("Vuosi ei ole karkausvuosi.")     #jos vuosiluku ei ole jaollinen neljällä, ei ole karkausvuosi
+    print("Vuosi ei ole karkausvuosi.")     #jos vuosiluku ei ole jaollinen neljällä, ei ole karkausvuosi.
 
 #yllä oleva toimii, mutta on varmasti parempi tapa ilman .is_integer() -komentoa????
 
