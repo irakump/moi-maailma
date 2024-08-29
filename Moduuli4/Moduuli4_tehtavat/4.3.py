788466dd17ce902b69c0ed3e19aafaeaeb4962e8
Ira <ira.kumpula@metropolia.fi>
2024-08-29 18:46:22 +0300

    4.3 kesken

--- Moduuli4/Moduuli4_tehtavat/4.3.py
+++ Moduuli4/Moduuli4_tehtavat/4.3.py
@@ -1,2 +1,13 @@
-# Tehtävä 4.3
-# Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi. Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
+"""
+Tehtävä 4.3
+Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän
+merkkijonon lopetusmerkiksi. Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
+"""
+number = float(input("Kerro luku: "))
+
+while (number != ""):           #ehto: muuttuja ei ole tyhjä merkkijono
+    number = ("Kerro luku: ")
+
+print(number)
+
+#miten voi yhdistää merkkijonon, onko string?
