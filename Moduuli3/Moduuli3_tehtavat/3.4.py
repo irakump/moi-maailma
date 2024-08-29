8043d848a0e32b68f8c7d95a76472d6c0927b4a4
Ira <ira.kumpula@metropolia.fi>
2024-08-29 09:00:19 +0300

    mod3 tehtävät

--- Moduuli3/Moduuli3_tehtavat/3.4.py
+++ Moduuli3/Moduuli3_tehtavat/3.4.py
@@ -6,6 +6,13 @@
 #KÄYTÄ JAKOJÄÄNNÖSTÄ: If-ehtoon - jos jakojäännös = 0, on x:llä jaollinen
 
 year = int(input("Kerro vuosiluku: "))      #kysytään vuosiluku
+four = (year % 4)                           #jaetaan luku neljällä, tuloksena jakojäännös
+
+if (four == 0):                             #ehto: jos jakojäännös on 0
+
+
+#vanha koodi, tee muutokset:
+year = int(input("Kerro vuosiluku: "))      #kysytään vuosiluku
 four = (year/ 4)                           #jaetaan vuosiluku neljällä
 
 if (four.is_integer()):             #kokeillaan, onko luku jaollinen neljällä #ei tarvi olla == True !
@@ -19,60 +26,3 @@ if (four.is_integer()):             #kokeillaan, onko luku jaollinen neljällä 
     else: print("Vuosi on karkausvuosi.")   #jos vuosiluku ei ole jaollinen sadalla, on silti karkausvuosi
 else:
     print("Vuosi ei ole karkausvuosi.")     #jos vuosiluku ei ole jaollinen neljällä, ei ole karkausvuosi.
-
-#yllä oleva toimii, mutta on varmasti parempi tapa ilman .is_integer() -komentoa????
-
-
-#testi2:
-"""
-if (onko_kokoluku == True):             #jos vuosiluku on jaollinen neljällä:
-    hundred = (year_first / 100)        #jaetaan sadalla
-    if (hundred.is_integer() == True):      #jos luku on jaollinen sadalla:
-        fourhundred = (year_first / 400):     #jaetaan vuosiluku 400:lla
-            if (fourhundred.is_integer() == True):
-                print("Vuosi on karkausvuosi.")     #tarviiko tätä ollenkaan?
-print("Vuosi on karkausvuosi.")
-else:
-    print("Vuosi ei ole karkausvuosi.")
-"""
-#voisiko käyttää komentoja and, or?
-
-#pitää testata, onko luku sadalla jaollinen JA? neljälläsadalla
-#  if (year_first >= 100):  # jos vuosiluku on suurempi kuin sata, tehdään tämä
-
-#lisää vielä testi, onko luku jaollinen sadalla.
-
-#print(year.is_integer())    #tulostuu true, jos on kokonaisluku
-
-#rivi 7: lasku antaa liukuluvun - ei voi muuttaa intiksi, koska silloin kaikki luvut olisivat kokonaislukuja.
-
-"""
-if(type(year) == int):
-    print("Vuosi on karkausvuosi.")
-else:
-    print("Vuosi ei ole karkausvuosi.")
-"""
-#print(type(year))   #tulostuu tyyppi: int/float
-
-#if(year / 4):                               #selvitetään, onko vuosi jaollinen neljällä
-    #print("Vuosi on karkausvuosi.")
-#else:
-    #print("Vuosi ei ole karkausvuosi.")
-
-# year / 4 ei toimi. Tulee olla kokonaisluku (int)
-# pitää selvittää lisäksi, onko vuosi jaollinen sadalla
-
-
-"""
-Selvitetään, onko luku int vai float:
-i = 100
-f = 1.23
-
-print(type(i))
-print(type(f))
-# <class 'int'>
-# <class 'float'>
-"""
-
-# print(f.is_integer())   #selvitetään, onko float-muotoinen luku kokonaisluku, esim. 10.0
-#f on muuttuja
---
+++ Moduuli3/testi_3.4.py
@@ -0,0 +1,78 @@
+# Tehtävä 3.4
+# Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi.
+
+# Vuosi on karkausvuosi, jos se on jaollinen neljällä. Sadalla jaolliset vuodet ovat karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
+
+#KÄYTÄ JAKOJÄÄNNÖSTÄ: If-ehtoon - jos jakojäännös = 0, on x:llä jaollinen
+
+year = int(input("Kerro vuosiluku: "))      #kysytään vuosiluku
+four = (year/ 4)                           #jaetaan vuosiluku neljällä
+
+if (four.is_integer()):             #kokeillaan, onko luku jaollinen neljällä #ei tarvi olla == True !
+    hundred = (year / 100)                  #vuosiluku jaetaan sadalla
+    if (hundred.is_integer()):      #kokeillaan, onko vuosiluku jaollinen sadalla
+        fourhundred = (year / 400)          #jaetaan vuosiluku 400:lla
+        if ( (hundred.is_integer()) and (fourhundred.is_integer()) ):  #jos jaollinen 100 ja 400, mol. totta
+            print("Vuosi on karkausvuosi.")
+        else:                               #jos sadalla jaollinen luku ei ole jaollinen 400:lla
+            print("Vuosi ei ole karkausvuosi.")
+    else: print("Vuosi on karkausvuosi.")   #jos vuosiluku ei ole jaollinen sadalla, on silti karkausvuosi
+else:
+    print("Vuosi ei ole karkausvuosi.")     #jos vuosiluku ei ole jaollinen neljällä, ei ole karkausvuosi.
+
+#yllä oleva toimii, mutta on varmasti parempi tapa ilman .is_integer() -komentoa????
+
+
+#testi2:
+"""
+if (onko_kokoluku == True):             #jos vuosiluku on jaollinen neljällä:
+    hundred = (year_first / 100)        #jaetaan sadalla
+    if (hundred.is_integer() == True):      #jos luku on jaollinen sadalla:
+        fourhundred = (year_first / 400):     #jaetaan vuosiluku 400:lla
+            if (fourhundred.is_integer() == True):
+                print("Vuosi on karkausvuosi.")     #tarviiko tätä ollenkaan?
+print("Vuosi on karkausvuosi.")
+else:
+    print("Vuosi ei ole karkausvuosi.")
+"""
+#voisiko käyttää komentoja and, or?
+
+#pitää testata, onko luku sadalla jaollinen JA? neljälläsadalla
+#  if (year_first >= 100):  # jos vuosiluku on suurempi kuin sata, tehdään tämä
+
+#lisää vielä testi, onko luku jaollinen sadalla.
+
+#print(year.is_integer())    #tulostuu true, jos on kokonaisluku
+
+#rivi 7: lasku antaa liukuluvun - ei voi muuttaa intiksi, koska silloin kaikki luvut olisivat kokonaislukuja.
+
+"""
+if(type(year) == int):
+    print("Vuosi on karkausvuosi.")
+else:
+    print("Vuosi ei ole karkausvuosi.")
+"""
+#print(type(year))   #tulostuu tyyppi: int/float
+
+#if(year / 4):                               #selvitetään, onko vuosi jaollinen neljällä
+    #print("Vuosi on karkausvuosi.")
+#else:
+    #print("Vuosi ei ole karkausvuosi.")
+
+# year / 4 ei toimi. Tulee olla kokonaisluku (int)
+# pitää selvittää lisäksi, onko vuosi jaollinen sadalla
+
+
+"""
+Selvitetään, onko luku int vai float:
+i = 100
+f = 1.23
+
+print(type(i))
+print(type(f))
+# <class 'int'>
+# <class 'float'>
+"""
+
+# print(f.is_integer())   #selvitetään, onko float-muotoinen luku kokonaisluku, esim. 10.0
+#f on muuttuja
