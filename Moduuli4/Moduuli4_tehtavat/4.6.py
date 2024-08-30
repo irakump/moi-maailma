9b74e91900867c7e93b8dbb96e115345a27c61d2
Ira <ira.kumpula@metropolia.fi>
2024-08-30 22:27:52 +0300

    Mod4 tehtävät

--- Moduuli4/Moduuli4_tehtavat/4.6.py
+++ Moduuli4/Moduuli4_tehtavat/4.6.py
@@ -9,14 +9,14 @@
 
 import random
 
-N = int(input("Syötä arvottavien pisteiden lukumäärä: "))   # kysytään pisteiden kokonaismäärä
-n = 0                                                       # ympyrään osuvien pisteiden lkm
+N = int(input("Syötä arvottavien pisteiden lukumäärä: "))   #kysytään pisteiden kokonaismäärä
+n = 0                                                       #ympyrään osuvien pisteiden lkm
 iterator = 0
 while iterator < N:
     iterator += 1
-    x = random.uniform(-1, 1)                            # arvotaan yksi piste (x,y)
+    x = random.uniform(-1, 1)                            #arvotaan yksi piste (x,y)
     y = random.uniform(-1, 1)
-    if x**2 + y**2 < 1:                                     # testataan, onko piste yksikköympyrässä
+    if x**2 + y**2 < 1:                                     #testataan, onko piste yksikköympyrässä
         n += 1
-pii = 4 * int(n) / int(N)                                   # lasketaan piin likiarvo
+pii = 4 * int(n) / int(N)                                   #lasketaan piin likiarvo
 print(f"Piin likiarvo on {pii}")
