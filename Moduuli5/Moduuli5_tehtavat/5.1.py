a253930ec2589d0cce3588c42bbb3ab93c5aa3a4
Ira <ira.kumpula@metropolia.fi>
2024-09-02 18:18:53 +0300

    Mod5 tehtäviä

--- Moduuli5/Moduuli5_tehtavat/5.1.py
+++ Moduuli5/Moduuli5_tehtavat/5.1.py
@@ -4,10 +4,18 @@
 # Käytä for-toistorakennetta.
 
 import random
+iterator = 0                            #kierrosmuuttuja
+dices = []                              #tyhjä lista noppien silmälukuja varten
 
 number = int(input("Syötä arpakuutioiden lukumäärä: "))
-print(number)
 
-dice1 = random.randint(1,6)
-print(dice1)
+while iterator < number:
+    dice = random.randint(1,6)
+    dices.append(dice)                  #lisätään nopan silmäluku listaan
+    iterator += 1                       #kasvatetaan kierrosmuuttujaa
 
+sum = 0
+
+for dice in dices:
+    sum = sum + dice                     #lisätään nopan silmäluku summa-muuttujaan
+print(f"Silmälukujen summa on {sum}.")
---
+++ Moduuli5/Moduuli5_tehtavat/5.2.py
@@ -0,0 +1,15 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
+# kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
+# Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen.
+# Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True.
+
+number = input("Kerro luku: ")          #merkkijono
+numbers = []
+
+while (number != ""):
+    number = int(number)                #muutetaan merkkijono luvuksi
+    numbers.append(number)              #lisätään luku listaan
+    number = input("Kerro luku: ")      #kysytään uusi luku (merkkijono)
+
+numbers.sort(reverse=True)              #järjestetään luvut suurimmasta pienimpään
+print(f"Viisi suurinta lukua ovat {numbers[0]}, {numbers[1]}, {numbers[2]}, {numbers[3]} ja {numbers[4]}.")
