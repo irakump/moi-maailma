ce526cdb4faea4927bff46978f53782c8d562065
Ira <ira.kumpula@metropolia.fi>
2024-09-12 15:00:37 +0300

    Mod7 tehtävät

--- Moduuli7/Moduuli7_tehtavat/7.3.py
+++ Moduuli7/Moduuli7_tehtavat/7.3.py
@@ -9,10 +9,12 @@
 # Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK. Löydät koodeja helposti selaimen avulla.)
 
 #alustetaan sanakirja, jossa avain on ICAO-koodi ja arvo on lentokentän nimi
-airport_dictionary = {"EFHK": "Helsinki-Vantaa Airport", "EFTP" : "Tampere-Pirkkala Airport", "EFRO" : "Rovaniemi Airport"}
+airport_dictionary = {"EFHK": "Helsinki-Vantaa Airport",
+                      "EFTP" : "Tampere-Pirkkala Airport", "EFRO" : "Rovaniemi Airport"}
 
 #kysytään käyttäjältä toiminto ensimmäisen kerran
-user_input = input("Valitse seuraavista (A, B tai C):\nA: syötä uusi lentoasema\nB: hae syötetyn lentoaseman tiedot\nC: lopeta\n")
+user_input = input("Valitse seuraavista (A, B tai C):"
+                   "\nA: syötä uusi lentoasema\nB: hae syötetyn lentoaseman tiedot\nC: lopeta\n")
 
 while (user_input != "C"):
     if (user_input == "A"):                                 #A: käyttäjä syöttää uuden lentoaseman
@@ -21,9 +23,10 @@ while (user_input != "C"):
         airport_dictionary[icao_code] = airport_name        #lisätään ICAO-koodi ja lentokenttä sanakirjaan
     elif (user_input == "B"):                               #B: haetaan syötetyn lentoaseman tiedot
         icao_code = input("Syötä lentoaseman ICAO-koodi: ")
-        print(f"Lentoaseman nimi on {airport_dictionary[icao_code]}.")       #tulostetaan arvo (lentoaseman nimi)
+        print(f"Lentoaseman nimi on {airport_dictionary[icao_code]}.")    #tulostetaan arvo (lentoaseman nimi)
 
     #kysytään käyttäjältä toiminto uudelleen
-    user_input = input("Valitse seuraavista (A, B tai C):\nA: syötä uusi lentoasema\nB: hae syötetyn lentoaseman tiedot\nC: lopeta\n")
+    user_input = input("Valitse seuraavista (A, B tai C):"
+                       "\nA: syötä uusi lentoasema\nB: hae syötetyn lentoaseman tiedot\nC: lopeta\n")
 
-print("Ohjelma lopetettu.")             #kun käyttäjä syöttää "C", ohjelma päättyy, ilmoitetaan siitä tulosteella.
+print("Ohjelma lopetettu.")            #kun käyttäjä syöttää "C", ohjelma päättyy, ilmoitetaan siitä tulosteella.
