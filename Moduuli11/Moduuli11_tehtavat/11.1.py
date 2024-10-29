4ed156d3303c3aa32efef1e4f72b373f68b540ab
Ira <ira.kumpula@metropolia.fi>
2024-10-29 21:19:10 +0200

    mod11 tehtävät

--- Moduuli11/Moduuli11_tehtavat/11.1.py
+++ Moduuli11/Moduuli11_tehtavat/11.1.py
@@ -1,9 +1,43 @@
 # Tehtävä 11.1
-# Toteuta seuraava luokkahierarkia Python-kielellä: Julkaisu voi olla kirja tai lehti. Jokaisella julkaisulla
-# on nimi. Kirjalla on lisäksi kirjoittaja ja sivumäärä, kun taas lehdellä on päätoimittaja. Kirjoita luokkiin
-# myös tarvittavat alustajat. Tee aliluokkiin metodi tulosta_tiedot, joka tudostaa kyseisen julkaisun kaikki
-# tiedot. Luo pääohjelmassa julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja
-# Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua). Tulosta molempien julkaisujen kaikki tiedot toteuttamiesi
-# metodien avulla.
+# Toteuta seuraava luokkahierarkia Python-kielellä: Julkaisu voi olla kirja tai lehti. Jokaisella julkaisulla on nimi.
+# Kirjalla on lisäksi kirjoittaja ja sivumäärä, kun taas lehdellä on päätoimittaja. Kirjoita luokkiin myös tarvittavat
+# alustajat. Tee aliluokkiin metodi tulosta_tiedot, joka tudostaa kyseisen julkaisun kaikki tiedot. Luo pääohjelmassa
+# julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua). Tulosta molempien
+# julkaisujen kaikki tiedot toteuttamiesi metodien avulla.
 
+# yliluokka
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
 
+    def tulosta_tiedot(self):
+        print(f"Julkaisun nimi: {self.nimi}")
+
+# aliluokka
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumaara):
+        super().__init__(nimi)
+        self.kirjoittaja = kirjoittaja
+        self.sivumaara = sivumaara
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Kirjoittaja: {self.kirjoittaja}\nSivumäärä: {self.sivumaara}")
+
+# aliluokka
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        super().__init__(nimi)
+        self.paatoimittaja = paatoimittaja
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Päätoimittaja: {self.paatoimittaja}")
+
+# pääohjelma
+lehti = Lehti("Aku Ankka", "Aki Hyyppä")
+kirja = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+
+lehti.tulosta_tiedot()
+print()
+kirja.tulosta_tiedot()
--- Moduuli11/Moduuli11_tehtavat/11.2.py
+++ Moduuli11/Moduuli11_tehtavat/11.2.py
@@ -1,8 +1,51 @@
 # Tehtävä 11.2
-# Kirjoita aiemmin laatimallesi Auto-luokalle aliluokat Sähköauto ja Polttomoottoriauto. Sähköautolla on
-# ominaisuutena akkukapasiteetti kilowattitunteina. Polttomoottoriauton ominaisuutena on bensatankin koko
-# litroina. Kirjoita aliluokille alustajat. Esimerkiksi sähköauton alustaja saa parametreinaan rekisteritunnuksen,
-# huippunopeuden ja akkukapasiteetin. Se kutsuu yliluokan alustajaa kahden ensin mainitun asettamiseksi sekä
-# asettaa oman kapasiteettinsa. Kirjoita pääohjelma, jossa luot yhden sähköauton (ABC-15, 180 km/h, 52.5 kWh) ja
-# yhden polttomoottoriauton (ACD-123, 165 km/h, 32.3 l). Aseta kummallekin autolle haluamasi nopeus, käske autoja
-# ajamaan kolmen tunnin verran ja tulosta autojen matkamittarilukemat.
+# Kirjoita aiemmin laatimallesi Auto-luokalle aliluokat Sähköauto ja Polttomoottoriauto. Sähköautolla on ominaisuutena
+# akkukapasiteetti kilowattitunteina. Polttomoottoriauton ominaisuutena on bensatankin koko litroina. Kirjoita
+# aliluokille alustajat. Esimerkiksi sähköauton alustaja saa parametreinaan rekisteritunnuksen, huippunopeuden ja
+# akkukapasiteetin. Se kutsuu yliluokan alustajaa kahden ensin mainitun asettamiseksi sekä asettaa oman kapasiteettinsa.
+# Kirjoita pääohjelma, jossa luot yhden sähköauton (ABC-15, 180 km/h, 52.5 kWh) ja yhden polttomoottoriauton (ACD-123,
+# 165 km/h, 32.3 l). Aseta kummallekin autolle haluamasi nopeus, käske autoja ajamaan kolmen tunnin verran ja tulosta
+# autojen matkamittarilukemat.
+
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, nopeus=0, kuljettu_matka=0):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = nopeus
+        self.kuljettu_matka = kuljettu_matka
+
+    def kiihdyta(self, nopeuden_muutos):
+        self.nopeus = self.nopeus + nopeuden_muutos
+        if self.nopeus < 0:
+            self.nopeus = 0
+        elif self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+
+    def kulje(self, tunnit):
+        self.kuljettu_matka = self.kuljettu_matka + self.nopeus * tunnit    # matka = nopeus * aika
+
+class Sahkoauto(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, akkukapasiteetti):
+        super().__init__(rekisteritunnus, huippunopeus)
+        self.akkukapasiteetti = akkukapasiteetti        # kilowattitunteina
+
+class Polttomoottoriauto(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, tankin_koko):
+        super().__init__(rekisteritunnus, huippunopeus)
+        self.tankin_koko = tankin_koko                  # litroina
+
+autot = []
+
+s = Sahkoauto("ABC-15", 180, 52.5)
+autot.append(s)
+
+p = Polttomoottoriauto("ACD-123", 165, 32.3)
+autot.append(p)
+
+s.nopeus = 150
+p.nopeus = 100
+s.kulje(3)
+p.kulje(3)
+
+for a in autot:
+    print(f"Auto {a.rekisteritunnus} on kulkenut {a.kuljettu_matka} km.")
