324dc315a3848c711aef55af9c3e558a2da71a64
Ira <ira.kumpula@metropolia.fi>
2025-01-14 15:53:03 +0200

    binäärijärjestelmästä

--- Digitaalitekniikka/Digitaalitekniikka.py
+++ Digitaalitekniikka/Digitaalitekniikka.py
@@ -1,4 +1,16 @@
 # Digitaalitekniikka
 
-print(3**0)         # kolme potenssiin 0
+n = 3
+print(n**0)         # kolme potenssiin 0
 
+####
+print()
+
+# nollat ja ykköset lukuina = binäärijärjestelmä
+
+binary_str = "101"      # laita merkkijonona
+decimal = int(binary_str, 3)        # potenssin kanta tässä on 3, yleisin kanta 2
+print(decimal)
+
+
+print(2**12)
